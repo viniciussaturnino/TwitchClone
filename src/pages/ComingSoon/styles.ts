ba581f5e1bdf4a5fc5aacc2a43fcc20918acaf61
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-09-11 16:17:13 -0300

    Update background

--- src/pages/ComingSoon/styles.ts
+++ src/pages/ComingSoon/styles.ts
@@ -1,4 +1,8 @@
 /* eslint-disable import/no-unresolved */
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background: ${colors.primary};
+`;
--- src/pages/Following/styles.ts
+++ src/pages/Following/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
 export const Container = styled.View`
-
+  flex: 1;
+  background: ${colors.primary};
 `;
